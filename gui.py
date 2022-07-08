eeb1351e357c4464e2001516b0ed2dc8a5649388
Heriberto <herijuache@gmail.com>
2022-07-07 22:15:20 -0700

    Refactored code. Window closes when button is pressed.

--- gui.py
+++ gui.py
@@ -1,83 +1,119 @@
-import tkinter as tk
+from tkinter import *
 
 FONT_NAME = "Arial"
-FONT_SIZE = 12
-FONT = (FONT_NAME, FONT_SIZE)
+FONT_SIZE = 20
+FONT = (FONT_NAME, FONT_SIZE, "bold")
 
+class GUI:
 
-# Use inheritance?
-class GUI():
     def __init__(self):
-        window = tk.Tk()
-        window.title("PDF Splitter")
-        self.ent_name_str = ''
-        self.ent_pages_str = ''
-        self.name = ''
-        self.number_of_pages = 1
-        self.frm_name()
-        self.frm_pages()
-        window.mainloop()
+        self.pdf_name = ""
+        self.number_to_split = 1
+        self.window = Tk()
+        self.window.title("PDF Splitter")
+        self.window.config(pady=25, padx=25)
+        self.window.eval("tk::PlaceWindow . center")
+        self.load_window()
+        self.window.mainloop()
 
-    def frm_name(self):
-        self.lbl_name()
-        self.ent_name()
+    def submit(self):
+        print(self.pdf_name)
+        print(self.number_to_split)
+        self.window.destroy()
 
-    def lbl_name(self):
-        label = tk.Label(
-            text="Enter PDF name",
-            background="white",
-            foreground="black",
-            font=FONT,
-            width=25
-        )
-        label.pack()
+    def load_window(self):
+        # Label - Name
+        label_name = Label(text="Name of PDF", font=FONT)
+        label_name.pack()
 
-    def ent_name(self):
-        self.ent_name_str = tk.Entry()
-        self.ent_name_str.config(
-            font=FONT,
-            width=30
-        )
-        self.ent_name_str.pack()
+        # Entry - Name
+        entry_name = Entry(width=20, font=("Arial", 16))
+        self.pdf_name = entry_name.get()
+        entry_name.focus()
+        entry_name.pack(pady=10)
 
-    def btn_name(self):
-        submit = tk.Button(text="Submit", command=self.name_submit)
-        submit.pack()
+        # Label - Pages
+        label_pages = Label(text="Split every # pages", font=FONT)
+        label_pages.pack(pady=10)
 
-    def get_number_of_pages(self):
-        return self.number_of_pages
+        # Entry - Pages
+        entry_pages = Entry(width=20, font=("Arial", 16))
+        self.number_to_split = entry_pages.get()
+        entry_pages.pack(pady=10)
 
-    def frm_pages(self):
-        self.lbl_pages()
-        self.ent_pages()
-        self.btn_pages()
+        # Button - Submit
+        btn_submit = Button(text="Submit", font=("Arial", 16), command=self.submit)
+        btn_submit.pack()
 
-    def lbl_pages(self):
-        label = tk.Label(
-            text="Number of pages per document?",
-            background="white",
-            foreground="black",
-            font=FONT,
-            width=25
-        )
-        label.pack()
 
-    def ent_pages(self):
-        self.ent_pages_str = tk.Entry()
-        self.ent_pages_str.config(
-            font=FONT,
-            width=30
-        )
-        self.ent_pages_str.pack()
+    # def __init__(self):
+    #     window = tk.Tk()
+    #     window.title("PDF Splitter")
+    #     self.ent_name_str = ''
+    #     self.ent_pages_str = ''
+    #     self.name = ''
+    #     self.number_of_pages = 1
+    #     self.frm_name()
+    #     self.frm_pages()
+    #     window.mainloop()
+    #
+    # def frm_name(self):
+    #     self.lbl_name()
+    #     self.ent_name()
+    #
+    # def lbl_name(self):
+    #     label = tk.Label(
+    #         text="Enter PDF name",
+    #         background="white",
+    #         foreground="black",
+    #         font=FONT,
+    #         width=25
+    #     )
+    #     label.pack()
+    #
+    # def ent_name(self):
+    #     self.ent_name_str = tk.Entry()
+    #     self.ent_name_str.config(
+    #         font=FONT,
+    #         width=30
+    #     )
+    #     self.ent_name_str.pack()
+    #
+    # def get_number_of_pages(self):
+    #     return self.number_of_pages
+    #
+    # def frm_pages(self):
+    #     self.lbl_pages()
+    #     self.ent_pages()
+    #     self.btn_pages()
+    #
+    # def lbl_pages(self):
+    #     label = tk.Label(
+    #         text="Number of pages per document?",
+    #         background="white",
+    #         foreground="black",
+    #         font=FONT,
+    #         width=25
+    #     )
+    #     label.pack()
+    #
+    # def ent_pages(self):
+    #     self.ent_pages_str = tk.Entry()
+    #     self.ent_pages_str.config(
+    #         font=FONT,
+    #         width=30
+    #     )
+    #     self.ent_pages_str.pack()
+    #
+    # def pages_submit(self):
+    #     num_pages = self.ent_pages_str.get()
+    #     name = self.ent_name_str.get()
+    #     # show_name = tk.Label(text=f'{name}', font=FONT)
+    #     # show_name.pack()
+    #     self.number_of_pages = num_pages
+    #     self.name = name
+    #
+    # def btn_pages(self):
+    #     submit = tk.Button(text="Submit", command=self.pages_submit)
+    #     submit.pack()
 
-    def pages_submit(self):
-        num_pages = self.ent_pages_str.get()
-        name = self.ent_name_str.get()
-        # show_name = tk.Label(text=f'{name}', font=FONT)
-        # show_name.pack()
-        self.number_of_pages = num_pages
-        self.name = name
-
-    def btn_pages(self):
-        submit = tk.Button(text="Submit", command=self.pages_submit)
-        submit.pack()
--- main.py
+++ main.py
@@ -1,44 +1,107 @@
 import os
 from PyPDF2 import PdfReader, PdfWriter
-from gui import GUI
+from tkinter import *
+
+FONT_NAME = "Arial"
+FONT_SIZE = 20
+FONT = (FONT_NAME, FONT_SIZE, "bold")
+
 
 # Creates a PdfReader object that we can:
 # - get the number of pages
 
+# DRAG AND DROP IMPLEMENTATION
 # try:
 #     droppedFile = sys.argv[1]
 #     FILE_NAME = str(droppedFile)
 # except IndexError:
 #     print("No file dropped")
-CURRENT_PATH = os.getcwd()
 
-window = GUI()
+# ------------------------- LOGIC SETUP ------------------------------- #
+def split_pdf(pdf_name, number_per_pdf):
+    current_path = os.getcwd()
 
-input_pdf = PdfReader(open(f"{CURRENT_PATH}\\{window.name}.pdf", "rb"))
+    try:
+        input_pdf = PdfReader(open(f"{current_path}\\{pdf_name}", "rb"))
+    except FileNotFoundError:
+        label_name.config(text="File not Found")
+        # load_window()
 
-# Prompt user for input greater than 0
-# number_of_pages = int(screen.numinput("Number of Pages", "How many pages per document?", default=2))
-number_of_pages = int(window.get_number_of_pages())
+    else:
+        # Keeps track of current_path pages needed to be separated
+        page_tracker = number_per_pdf
 
-if number_of_pages == 0 or number_of_pages is None:
-    print("Invalid number")
+        # Iterate through the doc until we hit the last page
+        for i in range(0, input_pdf.numPages, number_per_pdf):
+            output = PdfWriter()  # Blank PdfWriter object
+            # Keeps the track in range of index
+            if page_tracker > input_pdf.numPages:
+                difference = page_tracker - input_pdf.numPages
+                page_tracker -= difference
+            for y in range(i, page_tracker):
+                output.addPage(input_pdf.getPage(y))  # Add page to Write object
+            page_number = int((i / number_per_pdf) + 1)
+            # prints Object to current_path path
+            with open(f"{current_path}/document-page%s.pdf" % page_number, "wb") as outputStream:
+                output.write(outputStream)
+            # updates the number of pages
+            page_tracker += number_per_pdf
 
-else:
-    # Keeps track of current pages needed to be separated
-    page_tracker = number_of_pages
 
-    # Iterate through the doc until we hit the last page
-    for i in range(0, input_pdf.numPages, number_of_pages):
-        output = PdfWriter()  # Blank PdfWriter object
-        # Keeps the track in range of index
-        if page_tracker > input_pdf.numPages:
-            difference = page_tracker - input_pdf.numPages
-            page_tracker -= difference
-        for y in range(i, page_tracker):
-            output.addPage(input_pdf.getPage(y))    # Add page to Write object
-        page_number = int((i / number_of_pages) + 1)
-        # prints Object to current path
-        with open(f"{CURRENT_PATH}/document-page%s.pdf" % page_number, "wb") as outputStream:
-            output.write(outputStream)
-        # updates the number of pages
-        page_tracker += number_of_pages
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("PDF Splitter")
+window.config(pady=10, padx=10)
+window.eval("tk::PlaceWindow . center")
+
+
+def check_name(filename):
+    try:
+        if os.path.exists(filename):
+            if os.path.isfile(filename):
+                return True
+    except FileNotFoundError:
+        label_name.config(text="File not found")
+
+
+def check_pages():
+    # Check if the second prompt is an INT
+    try:
+        num_pages = int(entry_pages.get())
+        if num_pages > 0:
+            return True
+    except ValueError:
+        label_pages.config(text="Enter valid #")
+
+
+def submit():
+    name = entry_name.get() + ".pdf"
+    # Both need to return 'True' to split PDF
+    if check_name(name) and check_pages():
+        split_pdf(name, int(entry_pages.get()))
+        window.destroy()
+
+
+# Label - Name
+label_name = Label(text="Name of PDF", font=FONT)
+label_name.pack()
+
+# Entry - Name
+entry_name = Entry(width=20, font=("Arial", 16))
+entry_name.focus()
+entry_name.pack(pady=10)
+
+# Label - Pages
+label_pages = Label(text="Split every # pages", font=FONT)
+label_pages.pack(pady=10)
+
+# Entry - Pages
+entry_pages = Entry(width=20, font=("Arial", 16))
+entry_pages.pack(pady=10)
+
+# Button - Submit
+btn_submit = Button(text="Submit", font=("Arial", 16), command=lambda: submit())
+btn_submit.pack()
+
+window.mainloop()
